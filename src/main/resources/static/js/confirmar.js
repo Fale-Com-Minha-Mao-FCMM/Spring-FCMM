2ac0de7f76bc948ddf566df77b4cf653b4c20275
Bieldeid <gabrieldeid.android@gmail.com>
2023-05-18 08:27:55 -0300

    Começando a desenvolver o pop-up de exclusão 🔨

---
+++ src/main/resources/static/js/confirmar.js
@@ -0,0 +1,36 @@
+function confirmarExclusao(event) {
+  event.preventDefault();
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+
+  swalWithBootstrapButtons.fire({
+    title: 'Você term certeza que deseja excluir?',
+    text: "Essa ação é irreversível",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Sim!',
+    cancelButtonText: 'Não.',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Excluído!',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado',
+        'Sua operação foi cancelada.)',
+        'error'
+      )
+    }
+  })
+}
